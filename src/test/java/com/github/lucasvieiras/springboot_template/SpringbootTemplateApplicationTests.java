3de08e1dc6f4019be8a60f3d3facb26daa08e986
lucasvieiras <lucasvieirasilva.lpta@gmail.com>
2025-08-21 22:23:09 -0300

    chore: bootstrap project

---
+++ src/test/java/com/github/lucasvieiras/springboot_template/SpringbootTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lucasvieiras.springboot_template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
